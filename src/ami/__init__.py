c85f661ab48d60e0f9c678e6c9611942a4c74222
Brian Connelly <bdc@bconnelly.net>
2025-01-09 08:12:46 -0800

    Misc updates

--- src/ami/__init__.py
+++ src/ami/__init__.py
@@ -2,10 +2,13 @@ from importlib.metadata import version
 
 __version__ = version("ami")
 
+from .networking import online, using_host
 from .virtualenv import using_virtualenv
 from .vscode import using_vscode
 
 __all__ = [
+    "online",
+    "using_host",
     "using_virtualenv",
     "using_vscode",
 ]
--- src/ami/vscode.py
+++ src/ami/vscode.py
@@ -5,6 +5,7 @@ def using_vscode() -> bool:
     """Determine whether Visual Studio Code (or a derivative) is being used
 
     Returns:
-        bool: True if Visual Studio Code (or a derivative) is being used and False otherwise
+        bool: True if Visual Studio Code (or a derivative) is being used and False
+              otherwise
     """
     return using_envvar("TERM_PROGRAM", "vscode")
