3f52d5ee5482a0d64215bad7c8e9e2c099314958
Brian Connelly <bdc@bconnelly.net>
2025-02-16 08:46:13 -0800

    Add `using_uv()`

---
+++ src/ami/uv.py
@@ -0,0 +1,15 @@
+"""
+Module for detecting UV environments.
+"""
+
+from .envvar import using_envvar
+
+
+def using_uv() -> bool:
+    """
+    Check if running in a UV package or project.
+
+    Returns:
+        bool: True if running in UV, False otherwise.
+    """
+    return using_envvar("UV") 
